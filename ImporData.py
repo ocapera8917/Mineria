3b5e93cc45f7eaacc7b9370e2172bf7b0e211fa8
ocapera8917 <ojcapera@misena.edu.co>
2023-03-18 18:59:38 -0500

    Cambio de archivos

--- ImporData.py
+++ ImporData.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 #url="https://raw.githubusercontent.com/luisaarciniegas/auto/main/juegos2.csv"
 #url = "https://raw.githubusercontent.com/ocapera8917/Mineria/main/Captura_por_delito_sin_limpieza.csv"
-url = "G:\mineria\Proyecto\delitos_sexuales_sin_limpieza.csv"
+url = "G:\mineria\Proyecto\Captura_por_delito_sin_limpieza.csv"
 df  =  pd.read_csv(url)
-df.head(5)
+#df.dtypes
+print(df.dtypes)
