c0961325f306a7cfe5d16b57c91d2957463479dd
ocapera8917 <ojcapera@misena.edu.co>
2023-03-19 01:04:55 -0500

    Cambio de archivos

--- ImporData.py
+++ ImporData.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import re
 #url = "https://raw.githubusercontent.com/ocapera8917/Mineria/main/Captura_por_delito_sin_limpieza.csv"
 url = "G:\mineria\Proyecto\Reporte__Delitos_sexuales_Polic_sin_limpieza.csv"
 df  =  pd.read_csv(url)
 #df.loc[df['PAIS'].isna() , 'PAIS'] = 'COLOMBIA'
-df['FECHA HECHO'] = pd.to_datetime(df['FECHA HECHO'])
-df['DELITO'] = df['DELITO'].astype(str)
-df.to_csv('G:\mineria\Proyecto\Reporte__Delitos_sexuales_Polic_sin_limpieza.csv', index=False)
-print(df.dtypes)
-#print(df.isna().sum())
+#df['FECHA HECHO'] = pd.to_datetime(df['FECHA HECHO'])
+#df['DELITO'] = df['DELITO'].astype(str)
+#df.to_csv('G:\mineria\Proyecto\Reporte__Delitos_sexuales_Polic_sin_limpieza.csv', index=False)
+#print(df.dtypes)
+#print(df)
+print(df.isna().sum())
