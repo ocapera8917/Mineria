ab14b6764e56468a6a75701e6a983b84c9f07549
ocapera8917 <ojcapera@misena.edu.co>
2023-03-23 19:37:28 -0500

    Cambio de archivos

--- ImporData.py
+++ ImporData.py
@@ -9,4 +9,10 @@ df  =  pd.read_csv(url)
 #df.to_csv('G:\mineria\Proyecto\Reporte__Delitos_sexuales_Polic_sin_limpieza.csv', index=False)
 #print(df.dtypes)
 #print(df)
+#url = "G:\mineria\Proyecto\Captura_por_delito_sin_limpieza.csv"
+#url="https://raw.githubusercontent.com/luisaarciniegas/auto/main/juegos2.csv"
+#df.dtypes
+#df['FECHA'] = pd.to_datetime(df['FECHA'])
+#df.to_csv('G:\mineria\Proyecto\Captura_por_delito_sin_limpieza.csv', index=False)
+#print(df.dtypes)
 print(df.isna().sum())
